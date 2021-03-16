70ded8aa6f4872f236d242e06a8651f49f03a0da
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2021-03-16 15:43:59 +0530

    Added file system support and rendered file data

--- routes/index-route.js
+++ routes/index-route.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+const fs = require('fs')
+
 router.get("", (req, res) => {
     const data = {
         records: [
@@ -15,6 +17,45 @@ router.get("", (req, res) => {
         ]
     }
     res.render("index", data);
-  });
+});
+
+router.get("/data", (req, res) => {
+    fs.readFile('C:\\Users\\Neeyat\\Downloads\\Data.csv', (err, data) => { 
+        
+        if( err ) 
+            throw err;
+        else {
+            const fileContent = {records: []}
+
+            const arrayData = data.toJSON().data
+
+            for(let i = 0; i < arrayData.length; i++){
+                const rno = arrayData[i++]
+                const ch = arrayData[i++]
+                const os = arrayData[i++]
+                const dev = arrayData[i++]
+                const app = arrayData[i++]
+                const ip = arrayData[i++]
+                const clk_time = arrayData[i]
+                
+                fileContent.records.push(
+                    {
+                        rno: rno,
+                        ch: ch,
+                        os: os,
+                        dev: dev,
+                        app: app,
+                        ip: ip,
+                        clk_time: clk_time
+                    }
+                )
+                
+            }
+
+            res.render("index", fileContent);
+        }
+    })
+    
+})
 
 module.exports = router
